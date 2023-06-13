f8a55d84d5ea0e0ce2d8ab9b2256ad30b66f637f
Lily Springer <lilyspringer@hotmail.com>
2023-06-12 19:59:30 -0600

    header and footer styled

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,23 @@
 import React from "react";
 import { ImFacebook, ImInstagram, ImTwitter, ImGithub } from "react-icons/im";
+import styles from './Footer.module.css'
 
 const Footer = () => {
   return (
     <footer>
-      <div>
+      <div className={styles.socials}>
         <h5>Hit us up on our socials!</h5>
-        <div>
-          <a href="https://www.devmountain.com" target="_blank">
+        <div className={styles.icons}>
+          <a href="https://www.devmountain.com" target="_blank" className={styles.circle}>
             <ImFacebook size="1.5em" color="#3f3f3f" />
           </a>
-          <a href="https://www.devmountain.com" target="_blank">
+          <a href="https://www.devmountain.com" target="_blank" className={styles.circle}>
             <ImInstagram size="1.5em" color="#3f3f3f" />
           </a>
-          <a href="https://www.devmountain.com" target="_blank">
+          <a href="https://www.devmountain.com" target="_blank" className={styles.circle}>
             <ImTwitter size="1.5em" color="#3f3f3f" />
           </a>
-          <a href="https://www.devmountain.com" target="_blank">
+          <a href="https://www.devmountain.com" target="_blank" className={styles.circle}>
             <ImGithub size="1.5em" color="#3f3f3f" />
           </a>
         </div>
